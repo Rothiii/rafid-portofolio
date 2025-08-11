bee34613341869a5a0af0d3ae16dbe126d8dfca7
unknown <drome.emord@gmail.com>
2025-08-11 21:09:20 +0800

    Comment out the bridging section from hero to about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,19 +20,22 @@ const HomePage = () => {
               <br />
               <span className="text-accent">Rafid Al Khairy</span>
             </h1>
-            <p className="max-w-[480px] mb-9 text-white/80">
+            <p className="max-w-[480px] mb-5 text-white/80">
               I excel at crafting elegant digital experiences and I am
               proficient in various programming languages and technologies.
             </p>
             {/* button and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <div className="mb-8 xl:mb-0">
+              <div className="mb-5 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
                   iconStyles="w-8 h-8 border border-accent rounded-full flex items-center justify-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
                 />
               </div>
             </div>
+            {/* <p className="text-center text-white/80 mb-9">
+              Nice to meet you! Let's get to know me better.
+            </p> */}
           </div>
 
           {/* photo */}
@@ -42,15 +45,12 @@ const HomePage = () => {
         </div>
       </div>
 
-      <p className="text-center text-white/80 mb-8">
-        Nice to meet you! Let's get to know me better.
-      </p>
 
-      <div className="flex justify-center">
+      {/* <div className="flex justify-center">
         <div className="w-10 h-10 border-2 border-accent rounded-full flex items-center justify-center animate-bounce">
           <span className="text-accent">↓</span>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 };
