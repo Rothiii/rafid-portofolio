ced13f22d7b19b3cb15071cd3b705f575f2f70e4
unknown <drome.emord@gmail.com>
2025-08-23 03:57:17 +0800

    add subnavbar and fly navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,30 +3,33 @@ import Link from "next/link";
 // components
 import DesktopNav from "@/components/DesktopNav";
 import MobileNav from "@/components/MobileNav";
-import { Button } from "@/components/ui/button";
+import AssistTouch from "@/components/AssistTouch";
 
 const Header = () => {
   return (
-    <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto flex justify-between items-center">
-        {/* logo */}
-        <Link href="/">
-          <h1 className="text-5xl">
-            Rafid<span className="text-accent">.</span>
-          </h1>
-        </Link>
+    <>
+      <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+          {/* logo */}
+          <Link href="/">
+            <h1 className="text-5xl">
+              Rafid<span className="text-accent">.</span>
+            </h1>
+          </Link>
 
-        {/* desktop nav */}
-        <div className="hidden xl:flex items-center gap-8">
-          <DesktopNav />
-        </div>
+          {/* desktop nav */}
+          <div className="hidden xl:flex items-center gap-8">
+            <DesktopNav />
+          </div>
 
-        {/* mobile nav */}
-        <div className="xl:hidden">
-          <MobileNav />
+          {/* mobile nav */}
+          <div className="xl:hidden">
+            <MobileNav />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <AssistTouch />
+    </>
   );
 };
 
